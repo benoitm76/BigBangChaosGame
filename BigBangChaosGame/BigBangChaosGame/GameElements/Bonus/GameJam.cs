9b50d59a4c6675b30a713017c3a510247f97ed07
Benoit Mouquet <benoitm76@gmail.com>
2012-12-13 16:35:14 +0100

    C'est cool

---
+++ BigBangChaosGame/BigBangChaosGame/GameElements/Bonus/GameJam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace BigBangChaosGame
+{
+    class GameJam : Bonus
+    {
+        public GameJam(Vector2 size_window)
+            : base(size_window)
+        {
+        }
+
+        public override void LoadContent(Microsoft.Xna.Framework.Content.ContentManager content)
+        {
+            base.LoadContent(content, "logo_gamjam_bonus_v1.0");
+        }
+
+        public override void applyBonus(BBCGame bbcgame)
+        {
+            bbcgame.distance += 2000; 
+        }
+    }
+}
---
+++ BigBangChaosGame/BigBangChaosGame/GameElements/Game1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Threading.Tasks;
+using System.Threading;
+using ParticleEmitter;
+
+namespace BigBangChaosGame
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+
+        public Game1()
+        {
+            //Chargement des paramètres grapiques
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+            Content.RootDirectory = "Content";
+            var sceneMgr = new SceneManager(this);
+            Components.Add(sceneMgr);
+
+            new BackgroundScene(sceneMgr).Add();            
+            new MainMenuScene(sceneMgr).Add();
+        }
+
+        
+    }
+}
